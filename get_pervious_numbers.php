2d8b848cb9beff422d7ea429b4e7c76eab88b2b4
ahmed-99882 <77698923+ahmed-99882@users.noreply.github.com>
2021-08-30 20:08:29 +0100

    Update get_pervious_numbers.php

--- get_pervious_numbers.php
+++ get_pervious_numbers.php
@@ -1,27 +1,67 @@
 <?php
+/* ------------------first solution---------------------
 function getPreviousNumbers($nb) {
   if ($nb <= 0) {
+  
     echo "Negative Numbers & Zero Not Allowed"  . "\n";
+    
   } elseif ($nb > 10) {
     if ($nb % 2 == 0) {
+    
       for ($i = $nb; $i > 0; $i -= 2) {
         echo $i;
       }
       echo "\n";
+      
     } else {
+    
       echo $nb;
       for ($i = $nb - 1; $i > 0; $i -= 2) {
         echo $i;
       }
       echo "\n";
     } 
+    
   } else {
+  
+    for ($i = $nb; $i > 0; $i--) {
+      echo $i;
+    }
+    echo "\n";
+    
+  }
+} -----------------------------------------------------*/
+
+// -----------------second solution---------------------
+function getPreviousNumbers($nb) {
+  if ($nb <= 0) {
+    
+    echo "Negative Numbers & Zero Not Allowed"  . "\n";
+    
+  } elseif ($nb > 10 and $nb % 2 == 0) {
+    
+    for ($i = $nb; $i > 0; $i -= 2) {
+      echo $i;
+    }
+    echo "\n";
+    
+  } elseif ($nb > 10 and $nb % 2 == 1) {
+    
+    echo $nb;
+    for ($i = $nb - 1; $i > 0; $i -= 2) {
+      echo $i;
+    }
+    echo "\n";
+    
+  } else {
+    
     for ($i = $nb; $i > 0; $i--) {
       echo $i;
     }
     echo "\n";
   }
-}
+  
+}  // -------------------------------------------------
 
 // test cases 
 getPreviousNumbers(0); // "Negative Numbers & Zero Not Allowed"
