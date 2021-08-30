8c6d3ab1a4c58bdafaa7a6bb859238738189bd16
ahmed-99882 <77698923+ahmed-99882@users.noreply.github.com>
2021-08-30 19:42:38 +0100

    Create get_pervious_numbers.php

---
+++ get_pervious_numbers.php
@@ -0,0 +1,31 @@
+<?php
+function getPreviousNumbers($nb) {
+  if ($nb <= 0) {
+    echo "Negative Numbers & Zero Not Allowed"  . "\n";
+  } elseif ($nb > 10) {
+    if ($nb % 2 == 0) {
+      for ($i = $nb; $i > 0; $i -= 2) {
+        echo $i;
+      }
+      echo "\n";
+    } else {
+      echo $nb;
+      for ($i = $nb - 1; $i > 0; $i -= 2) {
+        echo $i;
+      }
+      echo "\n";
+    } 
+  } else {
+    for ($i = $nb; $i > 0; $i--) {
+      echo $i;
+    }
+    echo "\n";
+  }
+}
+
+// test cases 
+getPreviousNumbers(0); // "Negative Numbers & Zero Not Allowed"
+getPreviousNumbers(-20); // "Negative Numbers & Zero Not Allowed"
+getPreviousNumbers(10); // 10987654321
+getPreviousNumbers(17); // 17161412108642
+?>
